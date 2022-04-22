1cb41db77530c586fddac8929c8c2f22e1d0a6c9
asaadmartin <asaadmartin@localhost>
2022-04-22 14:15:17 -0400

    added junit test cases

--- large-scale-programming-assignments/src/org/howard/edu/lsp/finalExam/MapUtilitiesTest.java
+++ large-scale-programming-assignments/src/org/howard/edu/lsp/finalExam/MapUtilitiesTest.java
@@ -2,6 +2,8 @@ package org.howard.edu.lsp.finalExam;
 
 import org.howard.edu.lsp.finalExam.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.util.HashMap;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,9 +29,46 @@ public class MapUtilitiesTest {
 			System.out.println(map1);
 			System.out.println(map2);
 			
-			assertEquals(MapUtilities.CommonKeyValuePairs(map1, map2), 2);
-			MapUtilities.CommonKeyValuePairs(map1, map2);
-
+			assertEquals(MapUtilities.CommonKeyValuePairs(map1, map2), 1);
 
 	}
+	
+		@Test
+		@DisplayName("MapUtilities.isempty")
+		
+		public void testKeyValue1() throws NullMapException {HashMap<String, String> emptymap1= new HashMap<>();
+		assertTrue(emptymap1.isEmpty());
+				
+				HashMap<String, String> emptymap2= new HashMap<>();
+				assertTrue(emptymap2.isEmpty());
+				
+		}
+				
+	
+		
+		
+		
+		@Test
+		@DisplayName("MapUtilities.notequals key/value")
+		public void testKeyValue3() throws NullMapException {HashMap<String, String> map3= new HashMap<>();
+				map3.put("Alice", "Healthy");
+				map3.put("Mary", "Unhealthy");
+				map3.put("Bob", "Healthy");
+				map3.put("Chuck", "Fine");
+				map3.put("Felix", "Sick");
+				
+				
+				HashMap<String, String> map4= new HashMap<>();
+				map4.put("Mary", "Ecstatic");
+				map4.put("Felix", "Healthy");
+				map4.put("Ricardo", "Subperb");
+				map4.put("Tam", "Fine");
+				map4.put("Bob", "Happy");
+				
+				assertEquals(MapUtilities.CommonKeyValuePairs(map3, map4), 0);
+
+		}
+				
+				
+	
 }
